87fffd25ef624ca636bafadd43f2da7aca2b6ce8
etzorash-coder <et.rayan.d@eduiteh.eu>
2026-05-08 10:11:25 +0300

    h

--- my game/my game/obj/Release/net9.0/my game.AssemblyInfo.cs
+++ my game/my game/obj/Release/net9.0/my game.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("my game")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b366a5e4ed65f01706ccf17b3c5be01066f732be")]
 [assembly: System.Reflection.AssemblyProductAttribute("my game")]
